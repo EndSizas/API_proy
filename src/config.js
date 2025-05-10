212ccdc3cdcca35ebb2899c89efbf0996c85f048
api2024_2 <carlosryr24@gmail.com>
2025-05-10 14:55:32 -0500

    se actualizo la API

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import { config } from 'dotenv'
 config()
 
-export const BD_HOST = process.env.BD_HOST || "localhost";
-export const BD_DATABASE = process.env.BD_DATABASE || "bd_animales_resacatados";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+export const BD_HOST = process.env.BD_HOST || "bqmae0grsr8nx2sksvwk-mysql.services.clever-cloud.com";
+export const BD_DATABASE = process.env.BD_DATABASE || "bqmae0grsr8nx2sksvwk";
+export const DB_USER = process.env.DB_USER || "uocdeptssztnhmoq";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "ONVAXdfAqKVRp1f91X09";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || "proyecto123";
