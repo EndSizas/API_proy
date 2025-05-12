eee645e14981c1f15143dc2ae13bace4fa72af3b
api2024_2 <carlosryr24@gmail.com>
2025-05-12 13:46:51 -0500

    se actualizo animales

--- src/routes/animalRoutes.js
+++ src/routes/animalRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { reportAnimal, getAnimals, getAnimalDetails, updateAnimalLocation, addAnimalImages } from '../controladores/animalCtrl.js';
+import { reportAnimal, getAnimals, getAnimalDetails, updateAnimalLocation, addAnimalImages,getNoAssignedAnimals } from '../controladores/animalCtrl.js';
 import { verifyToken } from '../jwt/verifyToken.js';
 import { upload } from '../config/multer.js';
 
@@ -12,5 +12,6 @@ router.get('/', getAnimals); // Pública para ver animales
 router.get('/:id', getAnimalDetails); // Pública para ver detalles// Pública para ver detalles con ubicación
 router.post('/:id/locacion', verifyToken, updateAnimalLocation);
 router.post('/:id/imagenes', verifyToken, upload, addAnimalImages);
+router.get('/animales/no-asignados', getNoAssignedAnimals);
 
 export default router;
