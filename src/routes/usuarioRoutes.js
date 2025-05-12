7f3630faff4dbd32efb5d2bef8c98d5355cc20bc
api2024_2 <carlosryr24@gmail.com>
2025-05-12 12:23:23 -0500

    se dejo como estaba antes

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile, updateUserProfile,deleteUser,getCurrentUser } from '../controladores/usuarioCtrl.js';
+import { registerUser, loginUser, getUserProfile, updateUserProfile,deleteUser} from '../controladores/usuarioCtrl.js';
 import { verifyToken } from '../jwt/verifyToken.js';
 import { upload } from '../config/multer.js';
 const router = express.Router();
@@ -9,7 +9,6 @@ router.post('/register',upload, registerUser);
 router.post('/login', loginUser);
 
 // Rutas protegidas
-router.get('/me', verifyToken, getCurrentUser);
 router.get('/:id', verifyToken, getUserProfile);
 router.put('/perfil', verifyToken, updateUserProfile);
 router.delete('/:id', verifyToken, deleteUser);
